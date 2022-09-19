eabe5b554beef6dbbccce02fce3aa24749124ccd
hieuminh0524 <48044790+hieuminh0524@users.noreply.github.com>
2022-09-19 22:32:16 +0700

    Update

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-west-2-750310889403
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=https://udacityapi-env.eba-zkqhwpe9.us-east-1.elasticbeanstalk.com/
+export URL=https://udagramapidev-env.eba-zkqhwpe9.us-east-1.elasticbeanstalk.com/
